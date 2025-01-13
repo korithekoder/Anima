a50d5b0d01063d969c45109d872e73689d119d33
Kori <korithekoder@gmail.com>
2025-01-12 18:49:45 -0600

    added setup folder for future devs

---
+++ setup/basic-utils-setup.bat
@@ -0,0 +1,23 @@
+@echo off
+color 0a
+cd ..
+@echo on
+echo Installing basic libraries...
+echo This might take a few moments depending on your internet speed.
+echo Make sure that you have at least Haxe 4 installed for this .bat file to work!
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 5.6.1
+haxelib install flixel-addons 3.2.2
+haxelib install flixel-tools 1.5.1
+haxelib install hscript-iris 1.1.0
+haxelib install tjson 1.4.0
+haxelib install hxdiscord_rpc 1.2.4
+haxelib install hxvlc 1.9.2
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
+echo Finished basic library setup!
+echo Installing Microsoft Visual Studio Community...
+curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
+vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
+del vs_Community.exe
+pause
